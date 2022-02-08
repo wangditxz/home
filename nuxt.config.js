23c0fc7d91d2cc21ce51062876d2f241c2ff3029
wangdi <wangdi8@yiche.com>
2022-02-08 16:33:05 +0800

    feat: 修改配置

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,7 +2,7 @@ export default {
     // Target: https://go.nuxtjs.dev/config-target
     target: 'static',
     router: {
-        base: '/home/'
+        base: '/'
     },
     // Global page headers: https://go.nuxtjs.dev/config-head
     head: {
